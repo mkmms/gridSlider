53ad4778e7792c362df4f5539b4bd58fc93ea287
Mohankumar <mohankumar.m@ticketsimply.com>
2016-06-17 16:23:17 +0530

    first commit

---
+++ js/data.js
@@ -0,0 +1,44 @@
+var psDataSource = [
+  {   
+    gridHead: "ANTOFAGASTA Montt",
+    gridId: "ANTOFAGASTA",
+    gridImagePaths: ["images/Antofagasta_1.jpg", "images/La_Serena_1.jpg", "images/Iquique_1.jpg", "images/Valparaiso_1.jpg", "images/Copiapo_1.jpg", "images/Arica_1.jpg", "images/santiago_1.jpg"],
+    gridCitiesAndCost: [["ANTOFAGASTA","$35.000"], ["LA SERENA", "$15.000"], ["IQUIQUE", "$15.000"], ["VALPARAÍSO", "$15.000"], ["COPIAPÓ", "$15.000"], ["ARICA", "$15.000"], ["SANTIAGO", "$15.000"]]
+ },
+ {
+    gridHead: "LA SERENA",
+    gridId: "LA SERENA",
+    gridImagePaths: ["images/La_Serena_2.jpg", "images/Iquique_2.jpg", "images/Valparaiso_2.jpg", "images/Copiapo_2.jpg", "images/Arica_2.jpg", "images/santiago_2.jpg", "images/Antofagasta_2.jpg"],
+    gridCitiesAndCost: [["LA SERENA", "$15.000"], ["IQUIQUE", "$15.000"], ["VALPARAÍSO", "$15.000"], ["COPIAPÓ", "$15.000"], ["ARICA", "$15.000"], ["SANTIAGO", "$15.000"], ["ANTOFAGASTA","$35.000"]]
+ },
+ {
+    gridHead: "IQUIQUE",
+    gridId: "IQUIQUE",
+    gridImagePaths: ["images/Iquique_4.jpg", "images/Valparaiso_4.jpg", "images/Copiapo_4.jpg", "images/Arica_4.jpg", "images/santiago_4.jpg", "images/Antofagasta_4.jpg", "images/La_Serena_4.jpg"],
+    gridCitiesAndCost: [["IQUIQUE", "$15.000"], ["VALPARAÍSO", "$15.000"], ["COPIAPÓ", "$15.000"], ["ARICA", "$15.000"], ["SANTIAGO", "$15.000"], ["ANTOFAGASTA","$35.000"], ["LA SERENA", "$15.000"]]
+ },
+ {
+    gridHead: "VALPARAÍSO",
+    gridId: "VALPARAÍSO",
+     gridImagePaths: ["images/Valparaiso_5.jpg", "images/Copiapo_5.jpg", "images/Arica_5.jpg", "images/santiago_5.jpg", "images/Antofagasta_5.jpg", "images/La_Serena_5.jpg", "images/Iquique_5.jpg"],
+    gridCitiesAndCost: [["VALPARAÍSO", "$15.000"], ["COPIAPÓ", "$15.000"], ["ARICA", "$15.000"], ["SANTIAGO", "$15.000"], ["ANTOFAGASTA","$35.000"], ["LA SERENA", "$15.000"],["IQUIQUE", "$15.000"]]
+ },
+ {
+    gridHead: "COPIAPÓ",
+    gridId: "COPIAPÓ",
+    gridImagePaths: ["images/Copiapo_6.jpg", "images/Arica_6.jpg", "images/santiago_6.jpg", "images/Antofagasta_6.jpg", "images/La_Serena_6.jpg","images/Iquique_6.jpg", "images/Valparaiso_6.jpg"],
+    gridCitiesAndCost: [["COPIAPÓ", "$15.000"], ["ARICA", "$15.000"], ["SANTIAGO", "$15.000"], ["ANTOFAGASTA","$35.000"], ["LA SERENA", "$15.000"],["IQUIQUE", "$15.000"], ["VALPARAÍSO", "$15.000"]]
+ },
+  {
+    gridHead: "ARICA",
+    gridId: "ARICA",
+    gridImagePaths: ["images/Arica_3.jpg", "images/santiago_3.jpg", "images/Antofagasta_3.jpg", "images/La_Serena_3.jpg", "images/Iquique_3.jpg", "images/Valparaiso_3.jpg", "images/Copiapo_3.jpg"],
+    gridCitiesAndCost: [["ARICA", "$15.000"], ["SANTIAGO", "$15.000"], ["ANTOFAGASTA","$35.000"], ["LA SERENA", "$15.000"],["IQUIQUE", "$15.000"], ["VALPARAÍSO", "$15.000"], ["COPIAPÓ", "$15.000"]]
+ },
+  {
+    gridHead: "SANTIAGO",
+    gridId: "SANTIAGO",
+    gridImagePaths: ["images/santiago_7.jpg", "images/Antofagasta_7.jpg", "images/La_Serena_7.jpg", "images/Iquique_7.jpg", "images/Valparaiso_7.jpg", "images/Copiapo_7.jpg","images/Arica_7.jpg"],
+    gridCitiesAndCost: [["SANTIAGO", "$15.000"], ["ANTOFAGASTA","$35.000"], ["LA SERENA", "$15.000"],["IQUIQUE", "$15.000"], ["VALPARAÍSO", "$15.000"], ["COPIAPÓ", "$15.000"], ["ARICA", "$15.000"]]
+ }
+];
---
+++ js/gridSlider.js
@@ -0,0 +1,106 @@
+/*
+	MIT License
+*/
+(function ( $ ) {
+ 	"use strict";
+
+ 	$.gridSlider = function(gridWrap, options){
+
+ 		var _slider = this;
+
+ 		var defaults = {
+ 			data: []
+    }
+
+ 		_slider.singleGrid = function(){
+			_slider.singleObj = new Object(); 
+			_slider.gridWrap.find(".grid-slide").each(function(index, sGrid){
+				var _data = $(sGrid).data();
+				_slider.singleObj[index] = new Object();
+				_slider.singleObj[index]["element"] = sGrid;
+				$.each(_data, function(obj, data){
+					_slider.singleObj[index][obj] = data;
+				})
+			})
+
+			return _slider.singleObj;
+		}
+
+		_slider.captionAttach = function(capElement, source, _singleGrid, counter, isFirstTime){
+			var _settings = _slider.settings;
+			var capHtml = "";
+			if(_settings.data[source] != undefined && _settings.data[source].gridCitiesAndCost != undefined){
+				var currentData = _settings.data[source].gridCitiesAndCost;
+				if(isFirstTime){
+					var cap = currentData[0];
+				}else{
+					var cap = currentData[counter];
+				}
+				capHtml = cap[0] + "<div>" + cap[1] + "</div>";
+				$(capElement).html(capHtml);
+				$(_singleGrid["element"]).data("value", cap[0]);
+			}
+		}
+
+		_slider.imageAttach = function(imgElement, source, _singleGrid, counter, isFirstTime){
+			var _settings = _slider.settings;
+			if(_settings.data[source] != undefined && _settings.data[source].gridImagePaths != undefined){
+				var currentData = _settings.data[source].gridImagePaths;
+				if(isFirstTime){
+					var img = currentData[0];
+				}else{
+					var img = currentData[counter];
+				}
+				
+				$(imgElement).css("background-image", "url("+img+")");
+			}
+		}
+
+		_slider.attachEvents = function(imgElement, capElement, source, _singleGrid, counter, isFirstTime){
+			var _settings = _slider.settings;
+			_slider.captionAttach(capElement, source, _singleGrid, counter, true);
+			_slider.imageAttach(imgElement, source, _singleGrid, counter, true);
+			setInterval(function(){
+				_slider.captionAttach(capElement, source, _singleGrid, counter, false);
+				_slider.imageAttach(imgElement, source, _singleGrid, counter, false);
+				counter = (_settings.data.length - 1) > counter ? counter + 1 : 0
+			}, _singleGrid.sec)
+		}
+
+		_slider.contentSlide = function(_singleGrid){
+			var source = parseInt(_singleGrid["source"]);
+			var counter = {};
+			counter[source] = 0;
+			var capElement = $(_singleGrid["element"]).find(".grid-cap");
+			var imageElement = $(_singleGrid["element"]).find(".grid-img");
+			_slider.attachEvents(imageElement[0], capElement[0], source, _singleGrid, counter[source], true);
+		}
+
+		_slider.performSliding = function(){
+			$.each(_slider.singleGrid(), function(obj, _singleGrid){
+				_slider.contentSlide(_singleGrid);
+			})
+		}
+
+		// initialize settings and gridparent Element
+		_slider.init = function(){
+			_slider.settings = $.extend({}, defaults, options);
+			_slider.gridWrap = gridWrap;
+			_slider.performSliding();
+		}
+
+		_slider.init();
+ 	}
+ 	/*
+		==========================================
+		Grid Slider
+		==========================================
+		each tile will slide along with caption
+ 	*/
+
+  $.fn.gridSlider = function(options) {
+		var slider = new $.gridSlider(this, options);
+  };
+ 	
+
+}( jQuery ));
